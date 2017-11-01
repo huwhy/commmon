38ed5f8780528e3b2264cf68ea5e3fbbd411a14c
huwhy <huwhy@foxmail.com>
2017-11-01 11:03:26 +0800

    悲观锁接口

--- mybatis-util/src/main/java/cn/huwhy/ibatis/BaseDao.java
+++ mybatis-util/src/main/java/cn/huwhy/ibatis/BaseDao.java
@@ -10,19 +10,75 @@ import cn.huwhy.interfaces.Term;
 
 public interface BaseDao<T, PK extends Serializable> {
 
-    Long nextId();
-
-    int saves(@Param("list") Collection<T> list);
-
-    int update(T po);
-
+    /**
+     * 保存
+     *
+     * @param po
+     * @return
+     */
     int save(T po);
 
+    /**
+     * 批量保存
+     *
+     * @param list
+     * @return
+     */
+    int saves(@Param("list") Collection<T> list);
+
+    /**
+     * 更新
+     *
+     * @param po
+     * @return
+     */
+    int update(T po);
+
+    /**
+     * 批量更新
+     *
+     * @param pos
+     * @return
+     */
     int updates(@Param("list") Collection<T> pos);
 
+    /**
+     * 根据主键获取
+     *
+     * @param id
+     * @return
+     */
     T get(PK id);
 
+    /**
+     * 悲欢锁  实现sql 请带上 for update： select * from a where id=1 for update
+     *
+     * @param id
+     * @return
+     */
+    T lock(PK id);
+
+    /**
+     * 根据term 查询列表
+     *
+     * @param term
+     * @return
+     */
     List<T> findByTerm(Term term);
 
+    /**
+     * 分页查询列表
+     *
+     * @param term
+     * @return
+     */
     List<T> findPaging(Term term);
+
+    /**
+     * 下一个序列值
+     * 具体请查看readme
+     *
+     * @return
+     */
+    Long nextId();
 }
