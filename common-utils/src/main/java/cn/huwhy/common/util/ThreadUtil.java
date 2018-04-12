fddd07979f841142c0dcdaca3f3e7b5620f94f50
huwhy <huwhy@foxmail.com>
2018-04-12 10:43:12 +0800

    类型修改

--- common-utils/src/main/java/cn/huwhy/common/util/ThreadUtil.java
+++ common-utils/src/main/java/cn/huwhy/common/util/ThreadUtil.java
@@ -7,9 +7,25 @@ package cn.huwhy.common.util;
  */
 public class ThreadUtil {
 
-    public static void sleep(Long millis) {
+    public static void sleep(long millis) {
         try {
             Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException("thread interrupted");
+        }
+    }
+
+    public static void sleepSeconds(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException("thread interrupted");
+        }
+    }
+
+    public static void sleepMinute(int mins) {
+        try {
+            Thread.sleep(mins * 60 * 1000L);
         } catch (InterruptedException e) {
             throw new RuntimeException("thread interrupted");
         }
